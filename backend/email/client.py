324495eced61706d506901261fb542d03e79a219
Joshua Caponigro <jcaponigro20@gmail.com>
2023-10-19 00:39:14 -0400

    refactor to static class

--- backend/email/client.py
+++ backend/email/client.py
@@ -1,8 +1,8 @@
 from services.azure import AzureEmailCommunicationClient
 
 class EmailClient:
-    def __init__(self):
-        self.client = AzureEmailCommunicationClient
+    client = AzureEmailCommunicationClient
 
-    def send_email(self, to_email: str, subject: str, body: str, from_email: str = "tickets@scanbandz.com"):
-        return self.client.send_email(to_email=to_email, subject=subject, body=body, from_email=from_email)
+    @classmethod
+    def send_email(cls, to_email: str, subject: str, body: str, from_email: str = "tickets@scanbandz.com"):
+        return cls.client.send_email(to_email=to_email, subject=subject, body=body, from_email=from_email)
