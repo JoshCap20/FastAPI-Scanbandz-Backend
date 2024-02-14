b60116fff019eb1434b106379fc7d4964133991e
Josh Caponigro <joshcap@email.unc.edu>
2024-02-14 02:40:50 -0500

    Register host api

--- backend/backend/apis/hosts.py
+++ backend/backend/apis/hosts.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import JSONResponse
 
-from ..models.host import Host
+from ..models import BaseHost
 from ..services.host_service import HostService
 
 api = APIRouter(prefix="/api/hosts")
@@ -10,13 +10,19 @@ openapi_tags = {
     "description": "Host management.",
 }
 
-@api.get("/register", tags=["Hosts"])
-def register_host(host: Host, host_service: HostService = Depends()) -> JSONResponse:
+
+@api.post("/register", tags=["Hosts"])
+def register_host(
+    host: BaseHost, host_service: HostService = Depends()
+) -> JSONResponse:
     try:
         host = host_service.create(host)
     except ValueError as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
-    except Exception as e:
-        # TODO: Create specific exception for Host already exists
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Host already exists")
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content={"message": "Host registered successfully."})
+    # except Exception as e:
+    #     # TODO: Create specific exception for Host already exists
+    #     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Host already exists")
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content={"message": "Host registered successfully."},
+    )
