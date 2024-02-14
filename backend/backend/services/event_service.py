b82b43faa58f95c3a5177b131f906fa6ed50412e
Josh Caponigro <joshcap@email.unc.edu>
2024-02-14 03:23:11 -0500

    Event service type hints

--- backend/backend/services/event_service.py
+++ backend/backend/services/event_service.py
@@ -1,6 +1,6 @@
 from ..entities.event_entity import EventEntity
 from ..exceptions.event_exceptions import EventNotFoundException
-from ..models.event import Event
+from ..models import Event, BaseEvent, EventIdentity, Host
 from ..database import db_session
 
 from sqlalchemy.orm import Session
@@ -16,40 +16,40 @@ class EventService:
 
     def all(self) -> list[Event]:
         query = select(EventEntity)
-        entities = self._session.scalars(query).all()
+        entities: list[EventEntity] = self._session.scalars(query).all()
         return [entity.to_model() for entity in entities]
 
     def get_by_id(self, id: int) -> Event:
-        event_entity = self._session.get(EventEntity, id)
+        event_entity: EventEntity = self._session.get(EventEntity, id)
         if event_entity:
             return event_entity.to_model()
         else:
             raise EventNotFoundException(id)
-        
+
     def get_by_public_key(self, key: str) -> Event:
         query = select(EventEntity).where(EventEntity.public_key == key)
-        event_entity = self._session.scalars(query).first()
-        if event_entity:
-            return event_entity.to_model()
-        else:
-            raise EventNotFoundException(key)
-        
-    def get_by_private_key(self, key: str) -> Event:
-        query = select(EventEntity).where(EventEntity.private_key == key)
-        event_entity = self._session.scalars(query).first()
+        event_entity: EventEntity = self._session.scalars(query).first()
         if event_entity:
             return event_entity.to_model()
         else:
             raise EventNotFoundException(key)
 
-    def create(self, event: Event) -> Event:
-        event_entity = EventEntity.from_model(event)
+    def get_by_private_key(self, key: str) -> Event:
+        query = select(EventEntity).where(EventEntity.private_key == key)
+        event_entity: EventEntity = self._session.scalars(query).first()
+        if event_entity:
+            return event_entity.to_model()
+        else:
+            raise EventNotFoundException(key)
+
+    def create(self, event: BaseEvent, host: Host) -> Event:
+        event_entity: EventEntity = EventEntity.from_base_model(event, host.id)
         self._session.add(event_entity)
         self._session.commit()
         return event_entity.to_model()
 
     def delete(self, id: int) -> None:
-        event_entity = self._session.get(EventEntity, id)
+        event_entity: EventEntity = self._session.get(EventEntity, id)
         if event_entity:
             self._session.delete(event_entity)
             self._session.commit()
